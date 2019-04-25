52f85ecc80d9a741c2fa8d252464f5eecaead594
Lkgsr <LukasMahr@gmx.de>
2019-04-25 21:07:43 +0200

    Initial Commit

--- setup.py
+++ setup.py
@@ -3,13 +3,12 @@ from distutils.core import setup
 setup(
     name='Api-League-of-Legends',
     packages=['src'],
-    version='0.01',
+    version='0.02',
     license='MIT',
     description='Wrapper for the League of Legends Api',
     author='Lukas Mahr',
     author_email='LukasMahr@gmx.de',
     url='https://github.com/Lkgsr/League-of-Legends-Api',
-    download_url='https://github.com/Lkgsr/League-of-Legends-Api/archive/0.01.tar.gz',
     keywords=['API', 'Api', 'api', 'LeagueOfLegends', 'LeagueofLegends', 'league-of-legends', 'lol',
               'League-of-Legends', 'League-Of-Legends'],
     install_requires=[
@@ -19,9 +18,12 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
+    zip_safe=False,
+    package_dir={'': 'src'}
 )
